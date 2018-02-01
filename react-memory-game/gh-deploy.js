d2ae6f204190416d16ab46fa6164ee8f2857bdc0
mahy50 <934037783@qq.com>
2018-02-01 22:48:14 +0800

    fix; add; 修改配置，添加favicon

--- react-memory-game/gh-deploy.js
+++ react-memory-game/gh-deploy.js
@@ -1,7 +1,7 @@
 const ghpages = require('gh-pages')
 const path = require('path')
-ghpages.publish(path.join(__dirname, '../dist'), {
-  dest: 'vue'
+ghpages.publish(path.join(__dirname, './build'), {
+  dest: 'react'
 },function (err) {
   if (err) {
     console.log(err)
--- vue-memory-game/config/index.js
+++ vue-memory-game/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/memory-game/vue/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
